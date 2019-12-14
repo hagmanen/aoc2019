7ace10235d303731ecbb26d5bd61996ebf58e207
Fredrik Hagman <hagmanen@gmail.com>
2019-12-14 17:48:16 +0100

    Fix part 2

--- day12.py
+++ day12.py
@@ -2,6 +2,7 @@ import operator
 import itertools
 import re
 import copy
+import math
 
 def velocity_impact(p1, p2):
     if p1 == p2:
@@ -13,15 +14,15 @@ def velocity_impact(p1, p2):
 class moon():
     def __init__(self, pos):
         self.pos = pos
-        self.vel = (0, 0, 0)
+        self.vel = tuple([0] * len(pos))
 
     def interact(self, other):
-        velocity_change = (velocity_impact(self.pos[0], other.pos[0]), velocity_impact(self.pos[1], other.pos[1]), velocity_impact(self.pos[2], other.pos[2]))
-        self.vel = (self.vel[0] + velocity_change[0], self.vel[1] + velocity_change[1], self.vel[2] + velocity_change[2])
-        other.vel = (other.vel[0] - velocity_change[0], other.vel[1] - velocity_change[1], other.vel[2] - velocity_change[2])
+        velocity_change = tuple(map(velocity_impact, self.pos, other.pos))
+        self.vel = tuple(map(operator.add, self.vel, velocity_change))
+        other.vel = tuple(map(operator.sub, other.vel, velocity_change))
 
     def update_pos(self):
-        self.pos = (self.pos[0] + self.vel[0], self.pos[1] + self.vel[1], self.pos[2] + self.vel[2])
+        self.pos = tuple(map(operator.add, self.vel, self.pos))
 
     def print(self, name):
         print('%s pos %s vel %s' % (name, str(self.pos), str(self.vel)))
@@ -34,7 +35,7 @@ class moon():
 
 def parse_moon(line):
     coords = re.findall(r'[-+]?\d+', line)
-    return moon(tuple([int(x) for x in coords]))
+    return tuple([int(x) for x in coords])
 
 def print_moons(moons, iteration):
     id = 0
@@ -43,40 +44,60 @@ def print_moons(moons, iteration):
         moon.print('m' + str(id))
         id = id + 1
 
-def main():
-    filename = 'day12_input.txt'
-    with open(filename, 'r') as f:
-        text = f.read()
-    moons = []
-    for line in text.splitlines():
-        moons.append(parse_moon(line))
-    
+def part1(moons):
+    combinations = [x for x in itertools.combinations(moons, 2)]
+    for _ in range(0, 1000):
+        [m1.interact(m2) for m1, m2 in combinations]
+        [moon.update_pos() for moon in moons]
+            
+    print_moons(moons, 1000)
+    energy = 0
+    for moon in moons:
+        energy += moon.energy()
+    print('Total energy: %i' % energy)
+
+def get_period(moons):
+    combinations = [x for x in itertools.combinations(moons, 2)]
     original_moons = [copy.deepcopy(m) for m in moons]
     diff_moons = [x for x in zip(moons, original_moons)]
-    print_moons(moons, 0)
     it = 0
-    combinations = [x for x in itertools.combinations(moons, 2)]
-    print(combinations)
     while True:
         [m1.interact(m2) for m1, m2 in combinations]
         [moon.update_pos() for moon in moons]
             
         it = it + 1
-        if it == 1000:
-            print_moons(original_moons, 0)
-            print_moons(moons, it)
-            energy = 0
-            for moon in moons:
-                energy = energy + moon.energy()
-            print('Total energy: %i' % energy)
-        if not(it % 100000):
-            print('.', end = '', flush = True)
-        if not(it % 1000000):
-            print('')
-            print_moons(moons, it)
         if all(m0.equal(m1) for m0, m1 in diff_moons):
-            break
-    print_moons(moons, it)
+            return it
+
+def lcm(a, b):
+    return abs(a*b) // math.gcd(a, b)
+
+def part2(moonsx, moonsy, moonsz):
+    px = get_period(moonsx)
+    py = get_period(moonsy)
+    pz = get_period(moonsz)
+
+    print(lcm(lcm(px, py), pz))
+    
+
+def main():
+    filename = 'day12_input.txt'
+    with open(filename, 'r') as f:
+        text = f.read()
+    moons = []
+    moonsx = []
+    moonsy = []
+    moonsz = []
+    for line in text.splitlines():
+        pos = parse_moon(line)
+        moons.append(moon(pos))
+        moonsx.append(moon(tuple([pos[0]])))
+        moonsy.append(moon(tuple([pos[1]])))
+        moonsz.append(moon(tuple([pos[2]])))
+    
+    part1(moons)
+
+    part2(moonsx, moonsy, moonsz)
 
 if __name__ == "__main__":
     main()
