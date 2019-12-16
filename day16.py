23aca9e30f8779ab82422d5fd1816bf6b3cd2971
Fredrik Hagman <hagmanen@gmail.com>
2019-12-16 22:28:09 +0100

    Day 16 part 2

--- day16.py
+++ day16.py
@@ -16,18 +16,40 @@ def doit(imp, v):
         impn.append(row)
     return impn
 
-def main():
-    filename = 'day16_input.txt'
-    with open(filename, 'r') as f:
-        text = f.read().strip()
-
-    imp = [int(i) for i in text] #'80871224585914546619083218645595']
+def part1(text):
+    imp = [int(i) for i in text]
     v = [0, 1, 0, -1]
     print(imp[0:8])
     for i in range(0,100):
         imp = doit(imp, v)
         print(imp[0:8])
 
+
+def doitmore(work):
+    work2 = []
+    d = 0
+    for i in work:
+        d += i
+        d %= 10
+        work2.append(d)
+    return work2
+
+def main():
+    filename = 'day16_input.txt'
+    with open(filename, 'r') as f:
+        text = f.read().strip()
+    #text = '03036732577212944063491565474664' #84462026
+    #part1(text)
+
+    skip = int(text[0:7])
+    print(skip)
+    shitload = text * 10000
+    work = [int(i) for i in reversed(shitload[skip:])]
+    for _ in range(0,100):
+        work = doitmore(work)
+    for x in reversed(work[-8:]):
+        print(x, end = '')
+    print()
 
 if __name__ == "__main__":
     main()
