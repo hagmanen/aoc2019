613e3ac5c82042d149e78548cd6429c333e89a58
Fredrik Hagman <hagmanen@gmail.com>
2019-12-24 11:24:41 +0100

    Part 22 parse text before iter

--- day22.py
+++ day22.py
@@ -1,40 +1,55 @@
 import re
-import copy
 
-def m_cut(deck, nr):
-    if nr < 0:
-        nr = nr + len(deck)
-    return deck[nr:] + deck[0:nr]
+def read_instructions(text, deck_size):
+    shuffle = []
+    for line in text.splitlines():
+        if line == 'deal into new stack':
+            shuffle.append(lambda card: deck_size - card - 1)
+        elif m := re.match(r'cut (-?\d*)', line):
+            cut = int(m.group(1))
+            if cut < 0:
+                cut += deck_size
+            shuffle.append(lambda card, cut=cut: (card + deck_size - cut) if card < cut else (card - cut))
+        elif m := re.match(r'deal with increment (\d*)', line):
+            deal = int(m.group(1))
+            shuffle.append(lambda card, deal=deal: (card * deal) % deck_size)
 
-def m_deal(deck, nr):
-    new_deck = copy.copy(deck)
-    for x in range(0, len(deck)):
-        y = (x*nr) % len(deck)
-        new_deck[(x*nr) % len(deck)] = deck[x]
-    return new_deck
+    return shuffle
+
+def do_shuffle(card, shuffle):
+    for f in shuffle:
+        card = f(card)
+    return card
+
 
 def main():
     filename = 'day22_input.txt'
     with open(filename, 'r') as f:
         text = f.read()
+    
+    deck_size = 10007
+    shuffle = read_instructions(text, deck_size)
 
-    deck = [x for x in range(0, 10007)]
-    #deck = [x for x in range(0, 10)]
-    for line in text.splitlines():
-        if line == 'deal into new stack':
-            deck.reverse()
-        elif m := re.match(r'cut (-?\d*)', line):
-            deck = m_cut(deck, int(m.group(1)))
-        elif m := re.match(r'deal with increment (\d*)', line):
-            deck = m_deal(deck, int(m.group(1)))
+    card = 2019
+    print(do_shuffle(card, shuffle))
 
-    #print(deck)
-    #'''
-    count = 0
-    while deck[count] != 2019:
-        count += 1
-    print(count)
-    #'''
+    deck_size = 119315717514047
+    shuffle = read_instructions(text, deck_size)
+
+    card = 2020
+    tim = 101741582076661
+    #i = 1
+    #card = do_shuffle(card, shuffle)
+    #print('One shuffle')
+    #while card != 2020:
+    #    card = do_shuffle(card, shuffle)
+    #    i += 1
+    #print('Period = %i' % i)
+    #tim %= i
+    for i in range(0, tim):
+        card = do_shuffle(card, shuffle)
+        print(card)
+    print(card)
 
 if __name__ == "__main__":
     main()
