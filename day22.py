41b69bd56b1efb5910c6d192a16708f419a02d04
Fredrik Hagman <hagmanen@gmail.com>
2019-12-29 23:29:58 +0100

    Day 22 part 1 super fast

--- day22.py
+++ day22.py
@@ -1,36 +1,30 @@
 import re
-import copy
 
-def m_cut(deck, nr):
-    if nr < 0:
-        nr = nr + len(deck)
-    return deck[nr:] + deck[0:nr]
-
-def m_deal(deck, nr):
-    new_deck = copy.copy(deck)
-    for x in range(0, len(deck)):
-        y = (x*nr) % len(deck)
-        new_deck[(x*nr) % len(deck)] = deck[x]
-    return new_deck
+def op_combine(x, y):
+    (a, b) = x
+    (c, d) = y
+    return (a*c, b*c+d)
 
 def main():
     filename = 'day22_input.txt'
     with open(filename, 'r') as f:
         text = f.read()
-
-    deck = [x for x in range(0, 10007)]
+    op = (1, 0)
     for line in text.splitlines():
         if line == 'deal into new stack':
-            deck.reverse()
+            op = op_combine(op, (-1, -1))
         elif m := re.match(r'cut (-?\d*)', line):
-            deck = m_cut(deck, int(m.group(1)))
+            n = int(m.group(1))
+            op = op_combine(op, (1, -n))
         elif m := re.match(r'deal with increment (\d*)', line):
-            deck = m_deal(deck, int(m.group(1)))
+            n = int(m.group(1))
+            op = op_combine(op, (n, 0))
 
-    count = 0
-    while deck[count] != 2019:
-        count += 1
-    print(count)
+    deck_size = 10007
+    card = 2019
+    op = (op[0] % deck_size, op[1] % deck_size)
+    pos = (op[0] * card + op[1]) % deck_size
+    print(pos)
 
 if __name__ == "__main__":
     main()
