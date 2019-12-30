a1d6b7dcd223330ef9ec301fcb423e705f185630
Fredrik Hagman <hagmanen@gmail.com>
2019-12-30 19:31:01 +0100

    Fix day4 part 1

--- day4.py
+++ day4.py
@@ -1,26 +1,28 @@
 
-def next_password(password):
+def next_password(password, part2 = False):
     password = password + 1
     prev = int(password % 10)
     found_recuring = False
     for i in range(1, 6):
-        current = int((password % (10 ** (i + 1))) / (10 ** i))
+        current = (password % (10 ** (i + 1))) // (10 ** i)
         if prev <= current:
             found_recuring = True
         if prev < current:
-            keep = int(password / (10 ** (i + 1)))
-            password = int(str(keep) + str(current)*(i + 1))
+            keep = password - password % (10 ** (i + 1))
+            password = keep + int(str(current)*(i + 1))
         prev = current
     if (found_recuring):
         return password
     return next_password(password)
 
-def has_pair(password):
+def has_pair(password, part2 = False):
     prev = int(password % 10)
     found_recuring = 0
     for i in range(1, 6):
         current = int((password % (10 ** (i + 1))) / (10 ** i))
         if prev == current:
+            if not part2:
+                return True
             found_recuring = found_recuring + 1
         elif found_recuring == 1:
             return True
@@ -29,15 +31,21 @@ def has_pair(password):
             prev = current
     return found_recuring == 1
 
-def main():
+def calc_output(part2 = False):
     start = 178416
     stop = 676461
     count = 0
     while(start < stop):
-        start = next_password(start)
-        if(has_pair(start)):
+        if(has_pair(start, part2)):
             count = count + 1
-    print(count)
+        start = next_password(start, part2)
+    return count
+
+def main():
+    print('Part 1')
+    print(calc_output())
+    print('Part 2')
+    print(calc_output(True))
 
 #1650
 #1129
