fe8c6a24e6a6214533f2d72d09753f23539a46b2
Fredrik Hagman <hagmanen@gmail.com>
2019-12-30 19:49:00 +0100

    Fix day 17 part 1

--- day17.py
+++ day17.py
@@ -27,16 +27,13 @@ def main():
     m_scaffolds = set()
     m_intersections = set()
     while True:
-        #print('%i:%i' % (x, y))
         (m_exit, m_res) = m_computer.run()
         if m_exit == 99:
             break
         if m_res == 10:
             y += 1
             x = 0
-            #print()
             continue
-        #print(chr(m_res), end = '')
         if m_res == ord('#'):
             m_scaffolds.add((x, y))
         elif m_res == ord('.'):
@@ -50,38 +47,30 @@ def main():
         elif m_res == ord('>'):
             m_scaffolds.add((x, y))
         x += 1
-    #print()
     for m_scaffold in m_scaffolds:
         if f_is_intersection(m_scaffold, m_scaffolds):
-            #print('intersection %s' % (str(m_scaffold)))
             m_intersections.add(m_scaffold)
 
-    #print(m_scaffolds)
-    #print(m_intersections)
     m_sum = 0
     for m_intersection in m_intersections:
         m_sum += m_intersection[0] * m_intersection[1]
+    print('Part 1')
     print(m_sum)
 
     m_input_str = 'A,B,A,C,B,A,B,C,C,B\nL,12,L,12,R,4\nR,10,R,6,R,4,R,4\nR,6,L,12,L,12\nn\n'
     m_input = [ord(c) for c in m_input_str]
-    print(m_input)
 
     m_program[0] = 2
     m_computer = intcomputer(m_program, m_input)
-    m_sum = 0
     while True:
         (m_exit, m_res) = m_computer.run()
         if m_exit == 99:
             break
-        if m_res == 10:
-            print()
+        elif m_res == 10:
+            continue
         elif not str(chr(m_res)).isprintable():
-            print('[%i]' % m_res)
-        else:
-            print(chr(m_res), end = '')
-        m_sum += m_res
-    print(m_sum)
+            print('Part 2')
+            print('%i' % m_res)
 
 #5724
 #732985
